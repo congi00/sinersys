49c1c65203661fffb2aee52efdd9dba6c7a6e1f8
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2026-02-10 16:07:09 +0100

    Header & menu button containers

---
+++ app/components/Header.tsx
@@ -0,0 +1,20 @@
+export default function Header() {
+    return (
+      <div className="flex mt-6 
+        min-w-[84vw] 
+        min-h-[70px] 
+        rounded-full 
+        items-center 
+        justify-center 
+        p-4 
+        bg-[#F4F7FA]/20
+        backdrop-blur-xl
+        backdrop-saturate-150
+        rounded-3xl
+        border border-white/30
+        shadow-[0_8px_32px_rgba(0,0,0,0.15)]">
+        
+      </div>
+    );
+  }
+  
---
+++ app/components/MenuButton.tsx
@@ -0,0 +1,21 @@
+export default function MenuButton() {
+    return (
+      <div className="flex 
+        mb-6 
+        min-h-[90px] 
+        min-w-[90px]
+        rounded-full 
+        items-center 
+        justify-center 
+        p-4 
+        bg-[#F4F7FA]/20
+        backdrop-blur-xl
+        backdrop-saturate-150
+        rounded-3xl
+        border border-white/30
+        shadow-[0_8px_32px_rgba(0,0,0,0.15)]">
+        
+      </div>
+    );
+  }
+  
