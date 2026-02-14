14afabf3b8e630b55f00bec5e7d2fef4e99055d0
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2026-02-14 12:19:27 +0100

    [fix theme]

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default async function RootLayout({children}: Props) {
       <head>
         <meta name="apple-mobile-web-app-capable" content="yes"></meta>
         <meta name="theme-color" content="rgba(0,0,0,0)"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"></meta>
       </head>
       <body
         className={`
