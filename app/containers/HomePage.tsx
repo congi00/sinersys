71fb98bcecea58d224e3023e1f319cff2aa01845
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2026-02-16 11:13:38 +0100

    [fixes css]

--- app/containers/HomePage.tsx
+++ app/containers/HomePage.tsx
@@ -27,6 +27,7 @@ export default function HomePage({ progressMotion }: Props) {
           borderRadius,
         }}
         className={clsx(
+          "relative",
           "flex w-full",
           "items-center justify-center",
           "text-center",
