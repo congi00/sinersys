5798d0976c96081f63c083e31ca04dd38e5bb3a4
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2026-02-18 16:44:42 +0100

    [Fixed smartphone animation]

--- app/components/IntroParticles.tsx
+++ app/components/IntroParticles.tsx
@@ -131,32 +131,47 @@ export default function IntroParticles({ onFinish, showIntro }: Props) {
       <canvas ref={canvasRef} className="absolute inset-0" />
 
       {phase >= 4 && (
-        <div className="inset-0 flex flex-col items-center justify-center mt-[350px]">
-          {/* Full Logo */}
+        <div className="absolute inset-0 flex flex-col items-center justify-center px-6 text-center">
+          {/* Logo Blu */}
           <motion.img
             src="/logoblu.svg"
-            initial={{ opacity: 0, scale: 0.6, }}
-            animate={{ opacity: 1, scale: 1,  }}
-            exit={{
-              scale: 0,
-              opacity: 0,
-              transition: { duration: 0.1 },
-            }}
+            initial={{ opacity: 0, scale: 0.6 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ scale: 0, opacity: 0, transition: { duration: 0.1 } }}
             transition={{ duration: 2 }}
-            className="mb-6 w-[90px]"
+            className="
+              mb-4
+              w-[60px]
+              sm:w-[90px]
+            "
           />
+
+          {/* Full Logo */}
           <motion.img
             src="/full-logo-sinersys.png"
             initial={{ opacity: 0, scale: 0.6, filter: "invert(0)" }}
-            animate={{ opacity: 1, scale: 2.8, filter: "invert(1)" }}
+            animate={{ opacity: 1, scale: 2.6, filter: "invert(1)" }}
             exit={{ opacity: 0, scale: 0, transition: { duration: 0.1 } }}
             transition={{ duration: 1.5 }}
-            className="w-44"
+            className="
+              w-28
+              sm:w-44
+            "
           />
 
-          {/* H1 con effetto lettera per lettera */}
+          {/* H1 animato */}
           <motion.h1
-            className="text-3xl font-regular text-[#000000] mt-8 flex overflow-hidden"
+            className="
+              mt-6
+              flex
+              flex-wrap
+              justify-center
+              text-black
+              font-medium
+              tracking-wider
+              text-lg
+              sm:text-3xl
+            "
             initial="hidden"
             animate="visible"
             exit={{ opacity: 0, scale: 0, transition: { duration: 0.1 } }}
