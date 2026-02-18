67ac8b4d8013fa48a4aef2da3de7e8c5c22d974b
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2026-02-18 16:47:57 +0100

    [fixed smartphone style]

--- app/components/IntroParticles.tsx
+++ app/components/IntroParticles.tsx
@@ -150,7 +150,7 @@ export default function IntroParticles({ onFinish, showIntro }: Props) {
           <motion.img
             src="/full-logo-sinersys.png"
             initial={{ opacity: 0, scale: 0.6, filter: "invert(0)" }}
-            animate={{ opacity: 1, scale: 2.6, filter: "invert(1)" }}
+            animate={{ opacity: 1, scale: 2.7, filter: "invert(1)" }}
             exit={{ opacity: 0, scale: 0, transition: { duration: 0.1 } }}
             transition={{ duration: 1.5 }}
             className="
@@ -162,7 +162,8 @@ export default function IntroParticles({ onFinish, showIntro }: Props) {
           {/* H1 animato */}
           <motion.h1
             className="
-              mt-6
+              mt-4
+              sm:mt-6
               flex
               flex-wrap
               justify-center
