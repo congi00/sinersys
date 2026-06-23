64bd2653d5681d3cc50bde030f1c4b9ddbe1f94d
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2026-06-23 17:51:57 +0200

    [Fixed robots.ts]

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,25 @@
 import { MetadataRoute } from "next";
+
+  
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-        { userAgent: '*', allow: '/' },
-        { userAgent: 'Googlebot', allow: '/', disallow: ['/api/', '/_next/'] },
+      { 
+        userAgent: '*', 
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/server-sitemap.xml'],
+      },
+      { 
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: ['/api/', '/_next/'],
+      },
+      {
+        userAgent: ['AhrefsBot', 'SemrushBot', 'MJ12bot'],
+        disallow: '/',
+      },
     ],
-    sitemap: "https://www.sinersys.it/sitemap.xml",
+    sitemap: 'https://www.sinersys.it/sitemap.xml',
     host: 'https://www.sinersys.it',
   };
 }
-  
