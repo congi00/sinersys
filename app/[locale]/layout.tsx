1979fc8c49df33d7de46b737ab6199d9aa1b2379
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2026-07-06 13:35:30 +0200

    [Fixed duplicated title]

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -34,7 +34,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   return {
     title: {
       default: title,
-      template: "%s | Sinersys",
+      template: "%s",
     },
     description,
     keywords: [
