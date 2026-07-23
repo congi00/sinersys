b530599e23682813f32eea7122cd00315937cfe5
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2026-07-23 14:27:48 +0200

    [FIxed]

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -235,6 +235,7 @@ export default async function RootLayout({ children, params }: Props) {
         <a
           href="#main-content"
           className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[9999] focus:bg-white focus:text-black focus:p-2"
+          title="skip to main content"
         >
           Skip to main content
         </a>
