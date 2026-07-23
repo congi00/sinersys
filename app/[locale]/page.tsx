afdc46454b009e3d8386f10ee96b7aae34dbe844
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2026-07-23 14:23:09 +0200

    [Fixed metatitle]

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -83,7 +83,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 
   return {
-    title: 'Sinersys | ' + t('slide0.title'),
+    title: 'Sinersys | ' + t('slide0.metatitle'),
     description: t('slide0.subtitle'),
     keywords: t('keywords'),
     authors: [{ name: 'Sinersys' }],
